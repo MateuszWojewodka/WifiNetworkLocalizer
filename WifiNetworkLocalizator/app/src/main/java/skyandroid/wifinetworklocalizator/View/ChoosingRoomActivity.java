76951c706723127810712d0cf06f58f3c25b978a
skywatcher <mateusz.wojewodka.sky@gmail.com>
2018-04-08 17:51:11 +0200

    xml

---
+++ WifiNetworkLocalizator/app/src/main/java/skyandroid/wifinetworklocalizator/View/ChoosingRoomActivity.java
@@ -0,0 +1,8 @@
+package skyandroid.wifinetworklocalizator.View;
+
+/**
+ * Created by skywatcher_usr on 2018-04-08.
+ */
+
+public class ChoosingRoomActivity {
+}
---
+++ WifiNetworkLocalizator/app/src/main/java/skyandroid/wifinetworklocalizator/View/StartUpActivity.java
@@ -0,0 +1,47 @@
+package skyandroid.wifinetworklocalizator.View;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import skyandroid.wifinetworklocalizator.Model.HelperClasses.WifiDevicesDetails;
+import skyandroid.wifinetworklocalizator.R;
+
+public class StartUpActivity extends AppCompatActivity {
+
+    Button startButton;
+    Button adminButton;
+    Button exitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        startButton = (Button) findViewById(R.id.btnStart);
+        adminButton = (Button) findViewById(R.id.btnAdmin);
+        exitButton = (Button) findViewById(R.id.btnExit);
+
+
+    }
+
+    //View.OnClickListener
+
+}
